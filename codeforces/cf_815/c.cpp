9fd8f9082369b715bb5c5c8789e81664d610732d
Yogeesh <yryadappa100@gmail.com>
2022-08-19 01:44:19 +0530

    updates in c problem

--- codeforces/cf_815/c.cpp
+++ codeforces/cf_815/c.cpp
@@ -1,24 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+const int INF = 1e9;
+const int MAXN = 2000;
+int a[MAXN][MAXN];
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-        int n,m;
-        cin>>n>>m;
-        vector<vector<string> > s;
-        for (int i = 0; i < n; i++)
+        int n, m, sum = 0;
+        cin >> n >> m;
+        string s;
+        for (int i = 0; i < n; ++i)
         {
-            for (int j = 0; j < m; j++)
+            cin >> s;
+            for (int j = 0; j < m; ++j)
             {
-                cin>>s[i][j];
+                a[i][j] = s[j] - '0';
+                sum += a[i][j];
             }
-            
         }
-        int count=0;
-        
-        
+        int minn = INF;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = 0; j < m - 1; ++j)
+            {
+                int cnt = a[i][j] + a[i + 1][j] + a[i][j + 1] + a[i + 1][j + 1];
+                if (cnt == 0)
+                    continue;
+                minn = min(minn, max(1, cnt - 1));
+            }
+        }
+        if (sum == 0)
+            cout << "0\n";
+        else
+            cout << 1 + sum - minn << "\n";
     }
-    
 }
